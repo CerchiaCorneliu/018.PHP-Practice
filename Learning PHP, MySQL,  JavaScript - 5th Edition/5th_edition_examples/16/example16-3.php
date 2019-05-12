8be7520ca1ecd4c07226325c86b802e3e2cd6e20
CerchiaCorneliu <44298913+CerchiaCorneliu@users.noreply.github.com>
2019-05-12 22:05:39 +0300

    modify

--- Learning PHP, MySQL,  JavaScript - 5th Edition/5th_edition_examples/16/example16-3.php
+++ Learning PHP, MySQL,  JavaScript - 5th Edition/5th_edition_examples/16/example16-3.php
@@ -57,7 +57,7 @@
         fail += validatePassword(form.password.value)
         fail += validateAge(form.age.value)
         fail += validateEmail(form.email.value)
-      
+
         if (fail == "")     return true
         else { alert(fail); return false }
       }
@@ -121,7 +121,7 @@
           in your form: <p><font color=red size=1><i>$fail</i></font></p>
         </td></tr>
 
-      <form method="post" action="adduser.php" onSubmit="return validate(this)">
+      <form method="post" action="example16-3.php" onSubmit="return validate(this)">
         <tr><td>Forename</td>
           <td><input type="text" maxlength="32" name="forename" value="$forename">
         </td></tr><tr><td>Surname</td>
@@ -149,12 +149,12 @@ _END;
   {
   	return ($field == "") ? "No Forename was entered<br>": "";
   }
-  
+
   function validate_surname($field)
   {
   	return($field == "") ? "No Surname was entered<br>" : "";
   }
-  
+
   function validate_username($field)
   {
     if ($field == "") return "No Username was entered<br>";
@@ -162,9 +162,9 @@ _END;
       return "Usernames must be at least 5 characters<br>";
     else if (preg_match("/[^a-zA-Z0-9_-]/", $field))
       return "Only letters, numbers, - and _ in usernames<br>";
-    return "";		
+    return "";
   }
-  
+
   function validate_password($field)
   {
     if ($field == "") return "No Password was entered<br>";
@@ -176,7 +176,7 @@ _END;
       return "Passwords require 1 each of a-z, A-Z and 0-9<br>";
     return "";
   }
-  
+
   function validate_age($field)
   {
     if ($field == "") return "No Age was entered<br>";
@@ -184,7 +184,7 @@ _END;
       return "Age must be between 18 and 110<br>";
     return "";
   }
-  
+
   function validate_email($field)
   {
     if ($field == "") return "No Email was entered<br>";
@@ -194,7 +194,7 @@ _END;
         return "The Email address is invalid<br>";
     return "";
   }
-  
+
   function fix_string($string)
   {
     if (get_magic_quotes_gpc()) $string = stripslashes($string);
